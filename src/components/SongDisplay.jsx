92600194cb2b27cae08e7dabf97f1b0c630adc86
Student <example@example.com>
2020-03-03 11:13:41 -0800

    It took 20 attempts to commit this.

--- src/components/SongDisplay.jsx
+++ src/components/SongDisplay.jsx
@@ -9,21 +9,21 @@ const SongDisplay = ({ dispatch, song }) => {
   let action;
   return (
     <div>
-      <h1>{title}</h1>
-      <h4>{artist}</h4>
-      <hr/>
-      <div onClick={e => {
-        e.preventDefault();
-        if(!(arrayPosition === songArray.length - 1)) {
-          dispatch(nextLyric(id));
-        } else {
-          dispatch(restartSong(id));
-        }
-      }}>
-        <h1>
-          {currentLine}
-        </h1>
-      </div>
+    <h1>{title}</h1>
+    <h4>{artist}</h4>
+    <hr/>
+    <div onClick={e => {
+      e.preventDefault();
+      if(!(arrayPosition === songArray.length - 1)) {
+        dispatch(nextLyric(id));
+      } else {
+        dispatch(restartSong(id));
+      }
+    }}>
+    <h1>
+    {currentLine}
+    </h1>
+    </div>
     </div>
   );
 };
@@ -37,19 +37,27 @@ SongDisplay.propTypes = {
   arrayPosition: PropTypes.number,
   dispatch: PropTypes.func
 };
-
 const mapStateToProps = state => {
+  let info;
   const song = state.songsById[state.currentSongId];
-  const songInfo = {
-    id: song.songId,
-    artist: song.artist,
-    title: song.title,
-    songArray: song.songArray,
-    arrayPosition: song.arrayPosition
-  };
+  if (!state.songsById[state.currentSongId].isFetching) {
+    info = {
+      id: state.currentSongId,
+      artist: song.artist,
+      title: song.title,
+      songArray: song.songArray,
+      arrayPosition: song.arrayPosition
+    };
+  } else {
+    info = {
+      artist:'',
+      title: '',
+      songArray: '',
+      arrayPosition: ''
+    };
+  }
   return {
-    song: songInfo
+    song: info
   };
 };
-
 export default connect(mapStateToProps)(SongDisplay);
